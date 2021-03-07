355047936af6c7a89317ec1c6f32e1cde04b0572
KristenBoy1 <73790626+KristenBoy1@users.noreply.github.com>
2021-03-07 13:58:28 +0200

    vers001

---
+++ FoxTale1/Assets/Scripts/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+    public float minHeight, maxHeight;
+    public Transform farBg, midBg;
+    private float lastXPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastXPos = transform.position.x;  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float amountToMoveX = transform.position.x - lastXPos;
+        farBg.position = farBg.position + new Vector3(amountToMoveX, 0f, 0f);
+        midBg.position = midBg.position + new Vector3(amountToMoveX * 0.5f, 0f,0f);
+        lastXPos = transform.position.x;
+
+        transform.position = new Vector3(target.position.x, Mathf.Clamp(target.position.y,minHeight,maxHeight), transform.position.z);
+    }
+}
---
+++ FoxTale1/Assets/Scripts/MovingScript.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingScript : MonoBehaviour
+{
+    public float moveSpeed;
+    public Rigidbody2D rb;
+    public float jumpForce;
+    private bool isGrounded;
+    public Transform groundPoint;
+    public LayerMask whatisGround;
+    private bool canDoubleJump;
+    private Animator anim;
+    private SpriteRenderer sr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundPoint.position, .2f, whatisGround);
+        rb.velocity = new Vector2(moveSpeed*Input.GetAxis("Horizontal"), rb.velocity.y);
+        if (isGrounded)
+        {
+            canDoubleJump = true;
+        }
+        if (Input.GetButtonDown("Jump"))
+        {   
+
+            if (isGrounded)
+            {
+
+                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+                
+            }
+            else
+            {
+                if (canDoubleJump)
+                {
+                    rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+                    canDoubleJump = false;
+                }
+            }
+           
+        }
+
+        anim.SetFloat("speed", Mathf.Abs(rb.velocity.x));
+        anim.SetBool("isGrounded", isGrounded);
+        if (rb.velocity.x < 0)
+        {
+            sr.flipX = true;
+        }
+        else if (rb.velocity.x > 0)
+        {
+            sr.flipX = false;
+        }
+
+
+
+    }
+
+}
