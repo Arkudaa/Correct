5c3b17dd5a2c64673eec697c50c7dcf74699d35b
KristenBoy1 <73790626+KristenBoy1@users.noreply.github.com>
2021-03-14 13:52:52 +0200

    versioon2

---
+++ FoxTale1/Assets/Scripts/PlayerHealthController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealthController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int currentHealth, maxHealth;
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DealDamage()
+    {
+        currentHealth--;
+        if (currentHealth <=0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+}
