56f8dc34b01669a4181a7faa3164aacbd08e1ade
KristenBoy1 <73790626+KristenBoy1@users.noreply.github.com>
2021-03-21 17:43:08 +0200

    versioon5

---
+++ FoxTale1/Assets/checkpoint.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class checkpoint : MonoBehaviour
+{
+    public static checkpoint instance;
+    public SpriteRenderer sr;
+    public Sprite cpOn, cpOff;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.tag == "Player")
+        {
+            //Debug.Log("It works!");
+            cpController.instance.DeactivateCp();
+            sr.sprite = cpOn;
+            
+        }
+
+        
+
+
+        
+    }
+
+    public void Reset()
+    {
+        sr.sprite = cpOff;
+    }
+
+}
---
+++ FoxTale1/Assets/cpController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cpController : MonoBehaviour
+{
+    public static cpController instance;
+    private checkpoint[] checkpoints;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        checkpoints = FindObjectsOfType<checkpoint>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DeactivateCp()
+    {
+        for (int i=0; i< checkpoints.Length; i++)
+        {
+            checkpoints[i].Reset();
+        }
+    }
+}
